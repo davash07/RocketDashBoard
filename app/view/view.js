dbf3b63f25d386eb6f15b800b39e355cf6beea32
davash07 <davi9719@gmail.com>
2016-12-15 18:49:48 -0500

    commit

--- app/view/view.js
+++ app/view/view.js
@@ -6,54 +6,16 @@
         $scope.boards = [];
         $scope.quadroDefault= "q65dArv6";
         $scope.parseInt = parseInt;
+        $scope.CurrentDate = new Date();
         var token = '7624c37e41770770d71000f7f2dc13b9b1647cbb6bbe1c1ff28e214a0564fb5b';
-
         $http.get('https://api.trello.com/1/tokens/c4a90c8735f980c1257a90a8c205c03f58c971dc21aade2c3f6b66de1f6e8d1c/webhooks/?key=c8b7f2e9a6f82f88b47f767ed2122822')
         .success(function (data) {
             $scope.datos = data;
         });
-
-        // id.querySelector(".test").innerHTML = "yy";
-        // alert(angular.element("#test").val());
-        // alert(angular.element($document[0].querySelector("#test").value));
-        // $http.post('http://api.sundevs.com/api/v1/trellomodels/create', JSON.stringify(data)).then(function (data) {
-        //
-        // });
-        /*
-        setTimeout(function(){
-            window.location.reload();
-        }, 32000);*/
-        /*swal.queue([{
-            title: 'Your public IP',
-            confirmButtonText: 'Show my public IP',
-            text:
-            'Your public IP will be received ' +
-            'via AJAX request',
-            showLoaderOnConfirm: true,
-            preConfirm: function () {
-                return new Promise(function (resolve) {
-                    $.get('https://people.zoho.com/people/api/forms/P_TimesheetJobsList/getRecords?authtoken=6a701202eb76ebf85132b6ba39f6831d')
-                        .done(function (data) {
-                            swal.insertQueueStep(data.response.result)
-                            resolve()
-                        })
-                })
-            }
-        }]); */
-
-        $scope.url = "https://people.zoho.com/people/api/forms/P_TimesheetJobsList/getRecords?authtoken=6a701202eb76ebf85132b6ba39f6831d";
-        $http({
-            method: 'jsonp',
-            url: $scope.url,
-            params: {
-                format: 'jsonp',
-                name: 'Super Hero',
-                callback: 'JSON_CALLBACK'
-            }
-        }).then(function (response) {
-            alert(response.data);
-        });
         $scope.reload = function () {
+            $http.get('http://api.sundevs.com/api/v1/cards/quantity').success(function (data) {
+                $scope.quantity = data;
+            });
             $http.get('http://api.sundevs.com/api/v1/cards/show').success(function (data) {
                 if (data.card != null){
                     var audio = new Audio('sound.mp3');
@@ -63,7 +25,6 @@
                         type: 'error',
                         text: 'Se ha incluido un nuevo bug' ,
                         timer: 10000
-
                     }).then(
                         function () {},
                         function (dismiss) {
@@ -76,8 +37,6 @@
                     //  alert( data.card.members[0].fullname);
                     // // alert();alert
                 }
-
-
             });
 
             $timeout(function(){
@@ -85,8 +44,6 @@
             },3000)
         };
         $scope.reload();
-
-
         $scope.urlsimgs= "&authtoken=6a701202eb76ebf85132b6ba39f6831d";
         $scope.listOfCustomers = null;
 
@@ -195,56 +152,6 @@
         $scope.randomStacked();
     }]);
 
-    app.controller('calendarCntrl', ['$scope','$timeout', function($scope, $timeout) {
-        $scope.cards = [];
-        $scope.boards = [];
-        $scope.quadroDefault= "q65dArv6";
-        Trello.authorize({
-            name: "API-Test",
-            type: "popup",
-            interactive: true,
-            expiration: "never",
-            success: load,
-            scope: { write: true, read: true }
-        });
-        token = 'Trello.token()';
-        $scope.$watch(function () {
-            return Trello.authorized();
-        }, function (val) {
-            $scope.isLoggedIn = val;
-            $scope.tok = token;
-        });
-
-        function load() {
-            loadBoards();
-            loadCards();
-        }
-
-        function loadBoards() {
-            $scope.loadingBoards = true;
-            Trello.get("members/me/boards", function (boards){
-                angular.extend($scope.boards, boards);
-                $scope.loadingBoards = false;
-                $scope.poll();
-            });
-        }
-        function loadCards() {
-            $scope.loadingCards = true;
-            Trello.get("boards/"+ $scope.quadroDefault + "/cards/", function (cards){
-                angular.extend($scope.cards, cards);
-                $scope.loadingCards = false;
-                $scope.poll();
-            });
-        }
-        $scope.fresh = function(){
-            load();
-        };
-        $scope.poll = function() {
-            $timeout(function() {
-                console.log('poll');
-            }, 100);
-        };
-    }]);
     app.config(['$httpProvider', function($httpProvider) {
         $httpProvider.defaults.useXDomain = true;
         delete $httpProvider.defaults.headers.common['X-Requested-With'];
